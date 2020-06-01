9b4337fd5fc8bb1cd2d3af09f01ea7627aea1dbe
Alex Shabalin <alex.shabalin@pm.me>
2020-06-01 13:04:19 -0400

    Stuff in progress

--- elm-srcs.nix
+++ elm-srcs.nix
@@ -15,6 +15,26 @@
         version = "1.0.5";
       };
 
+      "elm/http" = {
+        sha256 = "008bs76mnp48b4dw8qwjj4fyvzbxvlrl4xpa2qh1gg2kfwyw56v1";
+        version = "2.0.0";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "elm/bytes" = {
+        sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
+        version = "1.0.8";
+      };
+
+      "elm/file" = {
+        sha256 = "1rljcb41dl97myidyjih2yliyzddkr2m7n74x7gg46rcw4jl0ny8";
+        version = "1.0.5";
+      };
+
       "elm/json" = {
         sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
         version = "1.1.3";
@@ -22,11 +42,6 @@
 
       "elm/url" = {
         sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
-        version = "1.0.0";
-      };
-
-      "elm/time" = {
-        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
         version = "1.0.0";
       };
 
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,131 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html)
+import Html.Events exposing (onClick)
+import Task
+import Time
+
+
+
+-- MAIN
 
 
 main =
-  text "Hello 4!"
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
+
+
+-- MODEL
+
+type TimerState
+  = WaitingSeconds Int
+  | RunningUntil Time.Posix
+  | Expired
+
+type alias CountdownTimerData =
+  { 
+
+  }
+
+type alias TimerProgram = List Int
+
+
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  , elapseAt : Maybe Time.Posix
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0) Nothing
+  , Cmd.none
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
+  | StartTimer
+
+plusSeconds : Int -> Time.Posix -> Time.Posix
+plusSeconds sec t1 = 
+  let millis = sec*1000 + Time.posixToMillis t1
+  in Time.millisToPosix millis
+
+timeDiff : Time.Posix -> Time.Posix -> Int
+timeDiff t2 t1 =
+  let 
+    t2m = Time.posixToMillis t2
+    t1m = Time.posixToMillis t1
+  in (t2m-t1m) // 1000
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      ( processModel { model | time = newTime }
+      , Cmd.none
+      )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
+
+    StartTimer ->
+      ( { model | elapseAt = Just (plusSeconds 5 model.time) }
+      , Cmd.none
+      )
+
+processModel : Model -> Model
+processModel model =
+  case model.elapseAt of
+    Nothing -> model
+    Just when ->
+      let
+        secondsLeft = timeDiff when model.time
+      in if secondsLeft < 0 then
+        { model | elapseAt = Nothing }
+      else model
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Time.every 50 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  case model.elapseAt of
+    Nothing ->
+      Html.div []
+        [
+          Html.button [ onClick StartTimer ] [ Html.text "Start" ]
+        ]
+
+    Just when ->
+      let
+        secondsLeft = timeDiff when model.time
+      in
+        Html.div []
+          [
+            Html.div [] [ Html.text (String.fromInt secondsLeft) ]
+          ]

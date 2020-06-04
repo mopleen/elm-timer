ebacadf48ddb254b5ea8625cbff0accb72d384ec
Alex Shabalin <alex.shabalin@pm.me>
2020-06-04 11:42:48 -0400

    Add release.nix
    Use nix formatter

--- auto.nix
+++ auto.nix
@@ -1,19 +1,10 @@
-{ nixpkgs
-, config ? {}
-}:
+{ nixpkgs, config ? { }, optimize ? false }:
 
 with (import nixpkgs config);
 
 let
-  mkDerivation =
-    { srcs ? ./elm-srcs.nix
-    , src
-    , name
-    , srcdir ? "./src"
-    , targets ? []
-    , registryDat ? ./registry.dat
-    , outputJavaScript ? false
-    }:
+  mkDerivation = { srcs ? ./elm-srcs.nix, src, name, srcdir ? "./src"
+    , targets ? [ ], registryDat ? ./registry.dat, outputJavaScript ? false }:
     stdenv.mkDerivation {
       inherit name src;
 
@@ -27,29 +18,36 @@ let
       };
 
       installPhase = let
-        elmfile = module: "${srcdir}/${builtins.replaceStrings ["."] ["/"] module}.elm";
+        elmfile = module:
+          "${srcdir}/${builtins.replaceStrings [ "." ] [ "/" ] module}.elm";
         extension = if outputJavaScript then "js" else "html";
+        optimizeOption = if optimize then "--optimize" else "";
       in ''
-        mkdir -p $out/share/doc
+        mkdir -p $out
         cp $src/src/index.html $out/
         cp -r $src/sounds $out/
         ${lib.concatStrings (map (module: ''
           echo "compiling ${elmfile module}"
-          elm make ${elmfile module} --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
-          ${lib.optionalString (outputJavaScript && false) ''
+          elm make ${
+            elmfile module
+          } ${optimizeOption} --output $out/${module}.${extension}
+          ${lib.optionalString (outputJavaScript && optimize) ''
             echo "minifying ${elmfile module}"
             uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
                 | uglifyjs --mangle --output=$out/${module}.min.${extension}
           ''}
         '') targets)}
+        ${lib.optionalString optimize ''
+          sed -i 's/Main.js/Main.min.js/' $out/index.html
+          rm $out/Main.js
+        ''}
       '';
     };
 in mkDerivation {
   name = "elm-app-0.1.0";
   srcs = ./elm-srcs.nix;
   src = ./.;
-  targets = ["Main"];
+  targets = [ "Main" ];
   srcdir = "./src";
   outputJavaScript = true;
 }
-
--- default.nix
+++ default.nix
@@ -1,7 +1,11 @@
-let src = import nix/sources.nix;
-pkgs = import src.nixpkgs {};
-derF = import ./auto.nix;
-der = derF { nixpkgs=src.nixpkgs; };
-in {
-  der = der;
-}
+{ optimize ? false }:
+
+let
+  src = import nix/sources.nix;
+  pkgs = import src.nixpkgs { };
+  derF = import ./auto.nix;
+  der = derF {
+    nixpkgs = src.nixpkgs;
+    inherit optimize;
+  };
+in { inherit der; }
